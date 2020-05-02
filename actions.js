1aca327de79a3eee41e8a1723604f0b4bb66d96f
Brady Vossler <bradyvossler@gmail.com>
2020-05-01 20:33:05 -0500

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,8 +1,8 @@
 function showHide() {
     const otherConditions = document.getElementById('other-conditions');
-    if (otherConditions.style.display === 'none') {
-        otherConditions.style.display = 'block';
-    } else {
+    if (otherConditions.style.display === 'block') {
         otherConditions.style.display = 'none';
+    } else {
+        otherConditions.style.display = 'block';
     }
 }
